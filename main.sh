907fa15394f3ecb3b3768963542d0ca140ce03ec
lampidis <loukas.lampidis@gmail.com>
2025-04-12 13:00:01 +0200

    minor

--- main.sh
+++ main.sh
@@ -2,10 +2,10 @@ wandb login
 
 python3 src/my_dinov2.py \
     --data-dir ./data/cityscapes \
-    --batch-size 64 \
+    --batch-size 16 \
     --epochs 60 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "dinov2_SGD" \
+    --experiment-id "dinov2_AdamW16" \
     --wandb-save True \
